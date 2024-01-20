7e357ddabd780d18d6c057ee1aebe9f16e3af1e9
gomathie <gomathie@byui.edu>
2024-01-20 04:34:06 +0300

    modified:   scripts/getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,15 @@
 let currentYear = new Date().getFullYear();
-document.querySelector('#currentYear').textContent = currentYear;
+
+let options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZoneName: 'short',
+    hour12: false,
+    hour: '2-digit',
+    minute: '2-digit'
+};
+document.querySelector('#currentYear').textContent = currentYear.toLocaleString('en-us', options);
 
 let lastModifiedDate = document.lastModified;
 document.querySelector('#lastModified').textContent = `Last modified: ${lastModifiedDate}`;
