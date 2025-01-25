1a68be3ea1266d7b12ae1457ce9c99fd3641c1c6
gomathie <gomathie@byui.edu>
2025-01-25 21:40:19 +0300

    modified:   index.html 	modified:   scripts/getdates.js 	modified:   styles/base.css 	modified:   styles/larger.css

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,13 @@ document.getElementById('currentYear').textContent = `${currentYear}`;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
+
+
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
