f1ab04bc8a31770bf842186061ab3a8cc469114c
gomathie <gomathie@byui.edu>
2025-02-09 03:36:45 +0300

    all

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,4 +1,8 @@
-function toggleMenu() {
-    document.querySelector(".nav-menu").classList.toggle("open");
-    document.querySelector(".hamburger").classList.toggle("active");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-links");
+
+    menuButton.addEventListener("click", function () {
+        navMenu.classList.toggle("active");
+    });
+});
